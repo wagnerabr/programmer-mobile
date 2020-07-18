8018ee455586a8e06087ff84f9a423ee34606273
Wagner <wagner.abr@hotmail.com>
2020-07-17 22:16:04 -0300

    Adjust button entrar and create readme

---
+++ README.md
@@ -0,0 +1,44 @@
+# README.md
+
+# API Programmers
+O objetivo deste app é a busca de programadores por tipo de tecnologia nas proximidades do usuário.
+
+
+## Fluxo de uso
+
+O fluxo de uso deste aplicativo é bem simples
+
+1. Dê permissão para o aplicativo usar a sua localização
+2. Faça o login com as credenciais [usuario: wagner, senha: 123456]
+3. Busque por uma tecnologia (exemplo, PHP)
+5. Selecione o usuário que aparecer para aparecer mais detalhes
+6. Selecione o balão de detalhes para aparecer a página do GitHub associada ao perfil
+
+A app consome dados de uma api hospedada no caminho https://programmers-api.herokuapp.com/
+Para verificar dados de usuários e tecnologias, consulte:
+* https://programmers-api.herokuapp.com/users
+* https://programmers-api.herokuapp.com/programmers
+
+## Prerequisites
+  - [Node,js](https://nodejs.org/en/) 10.19.0 or higher
+  - [NPM](https://www.npmjs.com/get-npm) 6.13.4 or higher
+  - [EXPO](https://expo.io/learn) Aplicativo instalado em seu celular
+
+## Getting started
+
+Clone este repositório em sua máquina local
+
+Utilize este comando para instalar as dependências:
+
+    npm install
+
+Para testar via Expo, use o seguinte comando:
+
+    npm start 
+    
+## Authors
+
+* **Camille Kamimura** - *Mathematical Modelling Analyst* - [CamilleKamimura](https://github.com/CamilleKamimura)
+* **Richard Felix** - *Data Specialist* - [riichfelix](https://github.com/riichfelix)
+* **Wagner Ribeiro** - *PHP developer* - [wagnerabr](https://github.com/wagnerabr)
+
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,34 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import {
-    Text,
-    TextInput,
-    Button,
-    View,
-    StyleSheet,
-    Dimensions
-  } from "react-native";
+  Text,
+  TextInput,
+  Button,
+  View,
+  StyleSheet,
+  Dimensions
+} from "react-native";
 
 import efetuarLogin from '../services/api-login';
 
 function Login({ navigation }) {
-    const [usuario, setUsuario] = useState("");
-    const [senha, setSenha] = useState("");
-    const [mensagemErro, setMensagemErro] = useState("")
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const [mensagemErro, setMensagemErro] = useState("")
 
-    const tentarLogar = async () => {
-        try {
-            
-          const token = await efetuarLogin(usuario, senha);
-        //   alert(token);
-        //   return false;
-        //   await AsyncStorage.setItem("instalura_token", token);
-          //Ir para tela Main
-          navigation.replace("Main", { nome: usuario })
-    
-        } catch (erro) {
-          setMensagemErro("Erro ao tentar logar")
-        }
-    
-      }
+  const tentarLogar = async () => {
+    try {
 
-    return (
+      const token = await efetuarLogin(usuario, senha);
+      //Ir para tela Main
+      navigation.replace("Main", { nome: usuario })
+
+    } catch (erro) {
+      setMensagemErro("Erro ao tentar logar")
+    }
+
+  }
+
+  return (
     <Fragment>
       <View style={styles.container}>
         <TextInput
@@ -45,33 +42,36 @@ function Login({ navigation }) {
           secureTextEntry={true}
           onChangeText={texto => setSenha(texto)}
         />
-        <Text>{mensagemErro}</Text>
+        <Text style={styles.errorText} >{mensagemErro}</Text>
       </View>
 
       <View style={styles.botaoView}>
-        <Button title="Entrar" onPress={tentarLogar} />
+        <Button title="Entrar" onPress={tentarLogar} color="#8E4Dff" />
       </View>
     </Fragment>
-    ); 
+  );
 }
 
 
 const largura = Dimensions.get("screen").width;
 const styles = StyleSheet.create({
-    container:{
-        flexGrow:2,
-        justifyContent:"center",
-        alignItems:"center"
-    },
-    inputs:{
-        width:largura*0.8,
-        textAlign:"center",
-        marginTop:40,
-        fontSize:30
-    },
-    botaoView:{
-        alignItems:"center",
-        marginBottom:50,
-    }
+  container: {
+    flexGrow: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputs: {
+    width: largura * 0.8,
+    textAlign: "center",
+    marginTop: 40,
+    fontSize: 30
+  },
+  botaoView: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  errorText: {
+    color: 'red'
+  }
 })
 export default Login;
